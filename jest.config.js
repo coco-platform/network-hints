e0234a47ba4f37b8e6994625bacb179a6cedbae4
huang.jian <hjj491229492@hotmail.com>
2019-05-10 17:07:20 +0800

    fix: correct coverage inspect source code

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
   },
   moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
   moduleDirectories: ['node_modules'],
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
   coverageDirectory: 'coverage',
   coverageReporters: ['lcov', 'html'],
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/__tests__/**/*.spec.{js,jsx,ts,tsx}'],
+  testMatch: ['<rootDir>/__tests__/**/*.spec.{js,jsx}'],
   testPathIgnorePatterns: ['/node_modules/'],
 };
